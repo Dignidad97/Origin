1798d930921344a304e85e680e76bdf4ec2b03d4
Dignidad97 <dignidad1945@gmail.com>
2022-08-22 00:27:27 +0300

    t

--- Chapter_2/name.py
+++ Chapter_2/name.py
@@ -1,2 +1,10 @@
 names = "vadym yatsun"
 print(names.title())
+print(names.upper())
+print(names.lower())
+
+first_name = '\nvadym'
+last_name = '\nyatsun'
+full_name = first_name + " " + last_name
+print("Hello, " + full_name.title() + "!")
+print("Does " + full_name + "\n\tis good boy?")
