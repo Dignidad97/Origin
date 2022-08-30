34f65c84b6122250cb1299b29dcc8ea9fcdd0b07
Dignidad97 <dignidad1945@gmail.com>
2022-08-30 17:40:21 +0300

    t

--- Chapter_3/3-1.py
+++ Chapter_3/3-1.py
@@ -1,2 +1,13 @@
 names = ['Andrew', 'Dima', 'Vlad']
-print(names[0])
+message = "Hi, " + names[0] + "! How are you?"
+print(message)
+
+message = "Hi, " + names[1] + "! How are you?" 
+print(message)
+
+message = "Hi, " + names[2] + "! How are you?"
+print(message)
+
+message = ["Дев'ятка", 'Форд', 'Метро']
+message = "Мій батя має авто, це  " + message[0] + ". Думаю, що б краще він мав" + message[1] ". Ну а мені, в принципі й на" + message[-1] + " норм)."
+print(message)
